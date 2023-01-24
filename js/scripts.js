53d8cfe5dfe246ac082e864bea23dd328a1a620c
micahglausier <122926346+micahglausier@users.noreply.github.com>
2023-01-24 08:17:06 -0600

    Added 3 pokemon, with name, height, and type to pokemonList variable

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,5 @@
 let pokemonList = [
-    
+    {name: 'Bulbasaur', height: '0.7', type: ['grass', 'poison']},
+    {name: 'Kangaskhan', height: '2.2', type: ['normal']},
+    {name: 'Gastly', height: '1.3', type: ['ghost', 'poison']} 
 ]
