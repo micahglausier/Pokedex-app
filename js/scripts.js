d234e4e3f6d5b51a898e98e8192350e567f65bbb
micahglausier <122926346+micahglausier@users.noreply.github.com>
2023-01-25 10:15:57 -0600

    Added new Pokemon

--- js/scripts.js
+++ js/scripts.js
@@ -3,12 +3,13 @@ let pokemonList = [
     //add Pokemon Objects, with Keys: name, height, and types//
     {name: 'Bulbasaur', height: '0.7', type: ['grass', 'poison']},
     {name: 'Kangaskhan', height: '2.2', type: ['normal']},
-    {name: 'Gastly', height: '1.3', type: ['ghost', 'poison']} 
+    {name: 'Gastly', height: '1.3', type: ['ghost', 'poison']},
+    {name: 'Golem', height: '1.4', ttpe: ['rock', 'ground']} 
 ]
 
 for (let i = 0; i <pokemonList.length; i++){
     document.write("<p>" + pokemonList[i].name + " (height: " + pokemonList[i].height +")");
-    if (pokemonList[i].height >= 1) {
-        console.log('Wow! that is a big Pokemon!') + "</p>"
+if (pokemonList[i].height >= 1.5) {
+    document.write(' - Wow! that is a big Pokemon!') + "</p>"
     }
 }
