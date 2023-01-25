516d7ceed14c1b8ec0e252c6d76aa2dd369f0fbb
micahglausier <122926346+micahglausier@users.noreply.github.com>
2023-01-25 17:51:02 -0600

    Added Function for displaying 2 sets of Pokemon Objects

--- js/scripts.js
+++ js/scripts.js
@@ -8,11 +8,20 @@ let pokemonList = [
     {name: 'Caterpie', height: '1', type: ['worm']}
 ]
 
+let pokemonList2 = [
+    // different set of Pokémon objects
+    {name: 'Butterfree', height: '3.07', type: ['bug', 'flying']},
+    {name: 'Nidoking', height: '4.07', type: ['ground', 'poison']},
+    {name: 'Blastoise', height: '5.03', type: ['water']},
+    {name: 'Ninetales', height: '3.07', type: ['fire']},
+    {name: 'Pikachu', height: '1.04', type: ['electric']}
+  ];
+
 //printArrayDetails function declaration
-function printArrayDetails(){
-    for (let i = 0; i < pokemonList.length; i++) {
-        document.write("<p>" + pokemonList[i].name + " (height: " + pokemonList[i].height +")");
-        if (pokemonList[i].height >= 1.5) {
+function printArrayDetails(list){
+    for (let i = 0; i < list.length; i++) {
+        document.write("<p>" + list[i].name + " (height: " + list[i].height +")");
+        if (list[i].height >= 1.5) {
             document.write(' - Wow! that is a big Pokemon!') + "</p>"  
         }
     }
@@ -20,8 +29,8 @@ function printArrayDetails(){
 
 
 // Calling printArrayDetails function twice
-printArrayDetails();
-printArrayDetails();
+printArrayDetails(pokemonList);
+printArrayDetails(pokemonList2);
 
 //for (let i = 0; i <pokemonList.length; i++){
 //    document.write("<p>" + pokemonList[i].name + " (height: " + pokemonList[i].height +")");
