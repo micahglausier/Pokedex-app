2de01df2cc9f300c03abba863d16e0dd66850ed5
micahglausier <122926346+micahglausier@users.noreply.github.com>
2023-01-25 10:01:51 -0600

    Added loop and rule to pokemonList

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,14 @@
+//pokemon list variable//
 let pokemonList = [
+    //add Pokemon Objects, with Keys: name, height, and types//
     {name: 'Bulbasaur', height: '0.7', type: ['grass', 'poison']},
     {name: 'Kangaskhan', height: '2.2', type: ['normal']},
     {name: 'Gastly', height: '1.3', type: ['ghost', 'poison']} 
 ]
+
+for (let i = 0; i <pokemonList.length; i++){
+    document.write("<p>" + pokemonList[i].name + " (height: " + pokemonList[i].height +")");
+    if (pokemonList[i].height >= 1) {
+        console.log('Wow! that is a big Pokemon!') + "</p>"
+    }
+}
