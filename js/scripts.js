8b9cb8b5daa10f362fce453825785171680ca9d6
micahglausier <122926346+micahglausier@users.noreply.github.com>
2023-01-30 14:05:48 -0600

    Added Repository and ForEach Function

--- js/scripts.js
+++ js/scripts.js
@@ -1,35 +1,69 @@
 //pokemon list variable//
-let pokemonList = [
-    //add Pokemon Objects, with Keys: name, height, and types//
-    { name: "Bulbasaur", height: "0.7", type: ["grass", "poison"] },
-    { name: "Kangaskhan", height: "2.2", type: ["normal"] },
-    { name: "Gastly", height: "1.3", type: ["ghost", "poison"] },
-    { name: "Golem", height: "1.4", type: ["rock", "ground"] },
-    { name: "Caterpie", height: "1", type: ["worm"] },
-];
+let pokemonRepository = (function () {
+    let pokemonList = [
+        //add Pokemon Objects, with Keys: name, height, and types//
+        { name: "Bulbasaur", height: "0.7", type: ["grass", "poison"] },
+        { name: "Kangaskhan", height: "2.2", type: ["normal"] },
+        { name: "Gastly", height: "1.3", type: ["ghost", "poison"] },
+        { name: "Golem", height: "1.4", type: ["rock", "ground"] },
+        { name: "Caterpie", height: "1", type: ["worm"] },
+        { name: "Butterfree", height: "3.07", type: ["bug", "flying"] },
+        { name: "Nidoking", height: "4.07", type: ["ground", "poison"] },
+        { name: "Blastoise", height: "5.03", type: ["water"] },
+        { name: "Ninetales", height: "3.07", type: ["fire"] },
+        { name: "Pikachu", height: "1.04", type: ["electric"] },
+    ]
 
-let pokemonList2 = [
-    // different set of Pokémon objects
-    { name: "Butterfree", height: "3.07", type: ["bug", "flying"] },
-    { name: "Nidoking", height: "4.07", type: ["ground", "poison"] },
-    { name: "Blastoise", height: "5.03", type: ["water"] },
-    { name: "Ninetales", height: "3.07", type: ["fire"] },
-    { name: "Pikachu", height: "1.04", type: ["electric"] },
-];
-
-//printArrayDetails function declaration
-function printArrayDetails(list) {
-    for (let i = 0; i < list.length; i++) {
-        document.write("<p>" + list[i].name + " (height: " + list[i].height + ")");
-        if (list[i].height >= 1.5) {
-            document.write(" - Wow! that is a big Pokemon!") + "</p>";
-        }
+    function add(pokemon) {
+        pokemonList.push(pokemon);
     }
-}
+
+    function getAll() {
+        return pokemonList;
+    }
+
+    return {
+        add: add,
+        getAll: getAll
+    };
+})();
+
+// let pokemonList2 = [
+    // different set of Pokémon objects
+//      { name: "Butterfree", height: "3.07", type: ["bug", "flying"] },
+//      { name: "Nidoking", height: "4.07", type: ["ground", "poison"] },
+//      { name: "Blastoise", height: "5.03", type: ["water"] },
+//      { name: "Ninetales", height: "3.07", type: ["fire"] },
+//      { name: "Pikachu", height: "1.04", type: ["electric"] },
+// ];
+
+//for each function
+pokemonList.forEach(function(pokemon) {
+    document.write("<p>" + pokemon.name + " (height: " + pokemon.height + ")" + " (type: " + pokemon.type + ")");
+});
+
+pokemonRepository.getAll(function(pokemon) {
+    document.write(pokemon.name +  pokemon.height);
+  });
+
+
+
+
+//old for function replaced with for each
+//printArrayDetails function declaration
+// function printArrayDetails(list) {
+//     for (let i = 0; i < list.length; i++) {
+//         document.write("<p>" + list[i].name + " (height: " + list[i].height + ")");
+//         if (list[i].height >= 1.5) {
+//             document.write(" - Wow! that is a big Pokemon!") + "</p>";
+//         }
+//     }
+// }
 
 // Calling printArrayDetails function twice
-printArrayDetails(pokemonList);
-printArrayDetails(pokemonList2);
+//printArrayDetails(pokemonList);
+//printArrayDetails(pokemonList2);
+
 
 //for (let i = 0; i <pokemonList.length; i++){
 //    document.write("<p>" + pokemonList[i].name + " (height: " + pokemonList[i].height +")");
@@ -37,5 +71,3 @@ printArrayDetails(pokemonList2);
 //    document.write(' - Wow! that is a big Pokemon!') + "</p>"
 //    }
 //}
-
-
