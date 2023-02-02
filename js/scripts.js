96fa0315abc48e21936038a6e78dce472e92ddf6
micahglausier <122926346+micahglausier@users.noreply.github.com>
2023-02-02 17:53:41 -0600

    Added loadDetails, Added modal, edited css for modal

--- js/scripts.js
+++ js/scripts.js
@@ -28,42 +28,116 @@ let pokemonRepository = (function () {
       });
     }
   
+    //load a list of pokemon from api. Promise fetch function.
     function loadList() {
-      return fetch(apiUrl).then(function (response) {
-        return response.json();
-      }).then(function (json) {
-        json.results.forEach(function (item) {
-          let pokemon = {
-            name: item.name,
-            detailsUrl: item.url
-          };
-          add(pokemon);
-          console.log(pokemon);
-        });
-      }).catch(function (e) {
-        console.error(e);
-      })
+        return fetch(apiUrl).then (function (response) {
+            // convert response to json
+            return response.json();
+        }).then (function (json) {
+            // api uses 'results' for array of pokemon. Each result, we are calling item. For each item, we assign keys to parameters (items from api)
+            json.results.forEach(function (item) {
+                let pokemon = {
+                    name: item.name,
+                    detailsUrl: item.url
+                };
+                // add function which pushes pokemon if it is object, and has name
+                add(pokemon);
+            });
+            // if any error occurs, it will be cought right here
+        }) .catch(function (e) {
+            console.error (e);
+        })
     }
-  
+    
+    
     function loadDetails(item) {
-      let url = item.detailsUrl;
-      return fetch(url).then(function (response) {
-        return response.json();
-      }).then(function (details) {
-        // Now we add the details to the item
-        item.imageUrl = details.sprites.front_default;
-        item.height = details.height;
-        item.types = details.types;
-      }).catch(function (e) {
-        console.error(e);
-      });
-    }
+        let url = item.detailsUrl;
+        return fetch(url).then(function (response) {
+          return response.json();
+        }).then(function (details) {
+          item.imageUrl = details.sprites.front_default;
+          item.height = details.height;
+          item.types = details.types;
+        }).catch(function (e) {
+          console.error(e);
+        });
+      }
   
-    function showDetails(item) {
-      pokemonRepository.loadDetails(item).then(function () {
-        console.log(item);
-      });
+    function showDetails (item) {
+        pokemonRepository.loadDetails(item).then (function () {
+            let modalContainer = document.querySelector ('#modal-container');
+    
+                modalContainer.innerHTML = '';
+    
+                let modal = document.createElement('div');
+                modal.classList.add ('modal');
+    
+                let sprite = document.createElement('img');
+                sprite.classList.add ('sprite');
+                sprite.src = item.imageUrl;
+    
+                let closeButtonElement = document.createElement ('button');
+                closeButtonElement.classList.add('modal-close');
+                closeButtonElement.innerText = 'X';
+                closeButtonElement.addEventListener ('click', hideModal)
+    
+                let titleElement = document.createElement ('h1');
+                titleElement.innerText =  (item.name);
+    
+                let contentElement = document.createElement ('p');
+    
+                // variable declared as empty string to be used to store the names of the types
+                let pokemonTypes = "";
+    
+                // for loop used to iterate through the item.types object.
+                for (let i = 0; i < item.types.length; i++) {
+                    //name of the current type is concatenated to the typeNames variable (appending to the end of the string)
+                    pokemonTypes += item.types[i].type.name;
+                    //if i is less than length - 1, a comma and space are added to typeNames (to avoid adding comma after las type)
+                    if (i < item.types.length - 1) {
+                        pokemonTypes += ", ";
+                    }
+                }
+    
+                // value of typeNames is then assigned to the innertext property of contentElement.
+                contentElement.innerText =('Height: ' + item.height + '\n' +  '\n' + 'Types: ' + pokemonTypes);
+    
+    
+                modal.appendChild (closeButtonElement);
+                modal.appendChild (titleElement);
+                modal.appendChild (contentElement);
+                modalContainer.appendChild (modal);
+                modal.appendChild (sprite);
+    
+    
+                modalContainer.classList.add('is-visible');
+    
+        
+            function hideModal (){
+                modalContainer.classList.remove ('is-visible');
+            }
+    
+            window.addEventListener('keydown', (e) => {
+                if (e.key === 'Escape' && modalContainer.classList.contains('is-visible')){
+                  hideModal();
+                }
+              });
+    
+            modalContainer.addEventListener('click', (e) => {
+            let target = e.target;
+            if (target === modalContainer) {
+                hideModal();
+            }
+            });
+    
+            document.querySelector ('button.button-class').addEventListener('click', () => {
+                showDetails ('Modal Title', 'Modal Content');
+            });
+    
+        });
+        
     }
+    
   
     return {
       add: add,
